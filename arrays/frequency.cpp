6d37786e0e11c9ba116ee960d1ffd55282cb5268
Kushal Singhal <kushalsinghal1210@gmail.com>
2026-01-06 21:51:18 +0530

    frequency of elements program
    Implement frequency counting of array elements.

---
+++ arrays/frequency.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n], visited[n] = {0};
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == 1) continue;
+
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                visited[j] = 1;
+            }
+        }
+        cout << arr[i] << " occurs " << count << " times" << endl;
+    }
+
+    return 0;
+}
