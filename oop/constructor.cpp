0be7745416bf20d71e9e15aed67f0e66cc2a4303
Kushal Singhal <kushalsinghal1210@gmail.com>
2026-01-12 17:13:40 +0530

    constructor example

---
+++ oop/constructor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    int age;
+
+    Person(string n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    Person p1("Ashi", 20);
+    p1.display();
+    return 0;
+}
