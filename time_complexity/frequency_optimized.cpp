ab31e4519767ac2ae34e91ac46dc4ffb9240cee3
Kushal Singhal <kushalsinghal1210@gmail.com>
2026-01-18 22:38:11 +0530

    Add optimized frequency program using unordered_map

---
+++ time_complexity/frequency_optimized.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    int n, x;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    unordered_map<int, int> freq;
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        freq[x]++;
+    }
+
+    cout << "Frequencies:\n";
+    for (auto p : freq) {
+        cout << p.first << " -> " << p.second << endl;
+    }
+
+    return 0;
+}
